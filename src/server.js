3a6cfbfdf27b1b77f98a9c6e70c26b5971ef5565
mxchacha <mxchacha@espe.edu.ec>
2020-09-10 07:01:00 -0500

    make

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ const app = express();
 require('./config/passport');
 
 // settings
-app.set('port', process.env.PORT || 4000);
+app.set('port', process.env.PORT || 3306);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('.hbs', exphbs({
   defaultLayout: 'main',
